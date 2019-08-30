f4c6df6b1254c4e4e875cc30940c0896475e3857
Adam Christopher <adam.keristoper@gmail.com>
2019-08-30 23:08:27 +0900

    Add basic menu. Will add handlers

--- ski_rental/cmd/main.go
+++ ski_rental/cmd/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func menu() {
+	fmt.Printf(`
+Welcome to Frosty Mumulala Snowsports rental!
+1. List all reservations
+2. Show reservation
+3. Create new reservation
+4. Update reservation
+5. Delete reservation
+`)
+}
+
+func readString(prompt string) string {
+	fmt.Printf("%s", prompt)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	var input string
+	fmt.Fscan(reader, &input)
+	return input
+}
+
+func app() {
+	sysInput := ""
+	for sysInput != "exit" {
+		menu()
+		sysInput = readString("Your Input: ")
+		if sysInput == "1" {
+			fmt.Println("You chose 1")
+		} else if sysInput == "2" {
+			fmt.Println("You chose 2")
+		} else {
+			sysInput = "exit"
+		}
+	}
+}
 
 func main() {
-	fmt.Println("hello bananas")
+	app()
 }
