f97b79cdae8c7787ec9bafa802aa4845b560555b
Adam Christopher <adam.keristoper@gmail.com>
2019-09-10 21:33:38 +0900

    Add ticker ping and set pong handler

--- websocket/client.go
+++ websocket/client.go
@@ -5,8 +5,23 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/websocket"
+)
+
+const (
+	// Time allowed to write a message to the peer.
+	writeWait = 10 * time.Second
+
+	// Time allowed to read the next pong message from the peer.
+	pongWait = 60 * time.Second
+
+	// Send pings to peer with this period. Must be less than pongWait.
+	pingPeriod = (pongWait * 9) / 10
+
+	// Maximum message size allowed from peer.
+	maxMessageSize = 512
 )
 
 var (
@@ -33,13 +48,19 @@ type Client struct {
 }
 
 func (c *Client) readPump() {
-	fmt.Println("read pump")
+	c.conn.SetReadLimit(maxMessageSize)
+	c.conn.SetReadDeadline(time.Now().Add(pongWait))
+	c.conn.SetPongHandler(func(a string) error {
+		fmt.Println("got pong", a)
+		return nil
+	})
 	for {
 		_, message, err := c.conn.ReadMessage()
 		if err != nil {
 			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
 				log.Printf("error: %v", err)
 			}
+			break
 		}
 		message = bytes.TrimSpace(bytes.Replace(message, newline, space, -1))
 		c.hub.broadcast <- message
@@ -47,21 +68,34 @@ func (c *Client) readPump() {
 }
 
 func (c *Client) writePump() {
+	ticker := time.NewTicker(pingPeriod)
+	defer func() {
+		ticker.Stop()
+		c.conn.Close()
+	}()
 	for {
-		message, ok := <-c.send
-		if !ok {
-			fmt.Println("not ok from client write pump")
-		}
+		select {
+		case message, ok := <-c.send:
+			if !ok {
+				fmt.Println("not ok from client write pump")
+			}
 
-		w, err := c.conn.NextWriter(websocket.TextMessage)
-		if err != nil {
-			return
-		}
+			w, err := c.conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
 
-		w.Write(message)
+			w.Write(message)
 
-		if err := w.Close(); err != nil {
-			return
+			if err := w.Close(); err != nil {
+				return
+			}
+		case <-ticker.C:
+			fmt.Println("ticker here")
+			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
+			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
 		}
 	}
 }
@@ -77,8 +111,6 @@ func serveWs(hub *Hub, w http.ResponseWriter, r *http.Request) {
 	client := &Client{hub: hub, conn: conn, send: make(chan []byte, 256)}
 
 	client.hub.register <- client
-
-	fmt.Println("registered client", client.hub.clients)
 
 	go client.readPump()
 	go client.writePump()
