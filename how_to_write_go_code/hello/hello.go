fb7e8c682af0738d1112721e55612f6a1ab8b5c0
Adam <adam.keristoper@gmail.com>
2019-09-19 22:02:38 +0900

    Creating a custom package

--- how_to_write_go_code/hello/hello.go
+++ how_to_write_go_code/hello/hello.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/shiningflint/go-gym/how_to_write_go_code/stringutil"
+)
 
 func main() {
 	fmt.Println("hello world")
+	fmt.Println(stringutil.Reverse("Bananas"))
 }
