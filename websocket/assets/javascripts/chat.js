c49522a57d5d74cc28c28a54fc550bac19b30102
Adam <adam.keristoper@gmail.com>
2019-10-12 15:36:07 +0900

    Change nickname and change color JS

--- websocket/assets/javascripts/chat.js
+++ websocket/assets/javascripts/chat.js
@@ -1,15 +1,19 @@
 function startChat () {
+  _changeNickNameFeature()
+  _changeTextColorFeature()
+
   let conn
   const log = document.getElementById("log")
 
   document.getElementById("form").addEventListener('submit', function (e) {
     e.preventDefault()
+    if (_stopSendOnInput()) { return false }
     const payload = {
       message: this.elements['message'].value,
       'user-id': this.elements['user-id'].value
     }
-    if (!conn) { return false; }
-    if (!payload['message'] || !payload['user-id']) { return false; }
+    if (!conn) { return false }
+    if (!payload['message'] || !payload['user-id']) { return false }
 
     conn.send(JSON.stringify(payload))
     this.elements['message'].value = ""
@@ -31,4 +35,51 @@ function startChat () {
   } else {
     console.log("no websocket")
   }
+}
+
+function _stopSendOnInput () {
+  return (document.activeElement.id && document.activeElement.id === 'nickname')
+}
+
+function _changeNickNameFeature () {
+  const nickShowElm = document.getElementById('chat-nickname-show')
+  const nickInputElm = document.getElementById('chat-nickname-input')
+  const nickShow = nickShowElm.firstElementChild
+  const nickInput = nickInputElm.firstElementChild
+  const updateNick = () => {
+    nickShow.innerText = nickInput.value
+    nickInput.blur()
+    nickShowElm.style.display = 'block'
+    nickInputElm.style.display = 'none'
+  }
+  nickShowElm.addEventListener('click', function (e) {
+    nickShowElm.style.display = 'none'
+    nickInputElm.style.display = 'block'
+    nickInput.addEventListener('focus', function (e) { e.target.select() })
+    nickInput.focus()
+  })
+  nickInput.addEventListener('keyup', function (e) {
+    if (e.keyCode === 13) { updateNick() }
+  })
+  nickInput.addEventListener('click', function (e) { updateNick() })
+}
+
+function _changeTextColorFeature () {
+  document.getElementById('btn-chat-color').addEventListener('click', function (e) {
+    e.preventDefault()
+    const elm = document.getElementById('color')
+    const textColors = ['#000000', '#4464AD', '#843B62' , '#36453B', '#FF785A']
+    const nextIndex = function () {
+      const currentIndex = textColors.includes(elm.value)
+        ? textColors.indexOf(elm.value)
+        : 0
+      if (currentIndex === (textColors.length - 1)) {
+        return 0
+      } else {
+        return currentIndex + 1
+      }
+    }()
+    elm.value = textColors[nextIndex]
+    e.target.style.backgroundColor = textColors[nextIndex]
+  })
 }
